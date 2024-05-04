718369794480d73651c38bb20295e2197a5b66b8
Rana Asad Ali <ranaasadaly@gmail.com>
2024-05-04 11:36:04 +0500

    ten

--- app/(components)/params/Params.tsx
+++ app/(components)/params/Params.tsx
@@ -1,14 +1,20 @@
-import { useSearchParams } from "next/navigation";
+// import { useSearchParams } from "next/navigation";
+// import React from "react";
+
+// export default function Params() {
+//   const searchParams = useSearchParams();
+
+//   if (!searchParams) {
+//     return null;
+//   }
+
+//   console.log(searchParams.get("title"));
+
+//   return <div>Params</div>;
+// }
+
 import React from "react";
 
 export default function Params() {
-  const searchParams = useSearchParams();
-
-  if (!searchParams) {
-    return null;
-  }
-
-  console.log(searchParams.get("title"));
-
   return <div>Params</div>;
 }
